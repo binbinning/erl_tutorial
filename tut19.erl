8bdfdcbb46ed534e7cbcbf56872368cdf8dbb2a6
Nick <binbinnjtu@163.com>
2017-12-04 15:32:36 +0800

    add tut19 timeout

---
+++ tut19.erl
@@ -0,0 +1,34 @@
+-module(tut19).
+
+-export([start_ping/1, start_pong/0,  ping/2, pong/0]).
+
+%%
+%% Time-outs based on tut17
+%%
+
+ping(0, Pong_Node) ->
+    io:format("ping finished~n", []);
+
+ping(N, Pong_Node) ->
+    {pong, Pong_Node} ! {ping, self()},
+    receive
+        pong ->
+            io:format("Ping received pong~n", [])
+    end,
+    ping(N - 1, Pong_Node).
+
+pong() ->
+    receive
+        {ping, Ping_PID} ->
+            io:format("Pong received ping~n", []),
+            Ping_PID ! pong,
+            pong()
+    after 5000 -> %waiting for a while
+            io:format("Pong timed out~n", [])
+    end.
+
+start_pong() ->
+    register(pong, spawn(tut19, pong, [])).
+
+start_ping(Pong_Node) ->
+    spawn(tut19, ping, [3, Pong_Node]).
